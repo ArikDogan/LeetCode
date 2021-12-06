3715de82b19a2fd1e4d58889b79b4648c7e2aaf5
ArikDogan <80952606+ArikDogan@users.noreply.github.com>
2021-12-06 13:42:42 +0300

    Create Two_Sum.c

---
+++ Two_Sum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i, j;
+    int *arr = (int*) malloc(sizeof(int)*2);
+    for(i = 0; i < numsSize; i++){
+        for(j = i + 1; j < numsSize; j++){
+            if(nums[i] + nums[j] == target){
+                *returnSize = 2;
+                arr[0] = i;
+                arr[1] = j;
+                return arr;
+                
+            }
+        }
+    }
+return arr;
+}
